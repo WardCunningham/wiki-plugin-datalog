6dc3d3b5c04e6e86df08d58184a886e6df40588d
Ward Cunningham <ward@c2.com>
2019-01-06 12:17:14 -0800

    record the clock

--- server/server.js
+++ server/server.js
@@ -2,16 +2,46 @@
 // These handlers are launched with the wiki server.
 
 (function() {
-  const startServer = params => {
+
+  const fs = require('fs');
+
+  function startRecorder(assets,slug) {
+
+    function mkdir(dir) {
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+      }
+    }
+
+    mkdir(`${assets}`)
+    mkdir(`${assets}/plugins`)
+    mkdir(`${assets}/plugins/datalog`)
+
+    function sample() {
+      let clock = Date.now()
+      let payload = JSON.stringify({clock})
+      let logfile = `${assets}/plugins/datalog/${slug}.log`
+      fs.appendFile(logfile, `${payload}\n`)
+    }
+
+    setInterval(sample,5000)
+
+  }
+
+
+  function startServer(params) {
     var app = params.app,
-        argv = params.argv;
+        argv = params.argv
+
+    startRecorder(argv.assets,'testing-datalog')
 
     return app.get('/plugin/datalog/:thing', (req, res) => {
-      var thing = req.params.thing;
-      return res.json({thing});
-    });
-  };
+      let thing = req.params.thing
+      let clock = Date.now()
+      return res.json({thing, clock})
+    })
+  }
 
-  module.exports = {startServer};
+  module.exports = {startServer}
 
-}).call(this);
+}).call(this)
