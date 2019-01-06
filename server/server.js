61f8e370fce76aa68c3d5e3b2e5562ba064ec9cf
Ward Cunningham <ward@c2.com>
2019-01-06 14:12:34 -0800

    new log every hour

--- server/server.js
+++ server/server.js
@@ -16,11 +16,29 @@
     mkdir(`${assets}`)
     mkdir(`${assets}/plugins`)
     mkdir(`${assets}/plugins/datalog`)
+    mkdir(`${assets}/plugins/datalog/${slug}`)
+
+    function decimal(number, digits) {
+      result = []
+      for (var i = 0; i < digits; i++) {
+        result.push(number % 10)
+        number = Math.floor(number / 10)
+      }
+      return result.reverse().join('')
+    }
+
+    function utc (date) {
+      let y = decimal(date.getUTCFullYear(), 4)
+      let m = decimal(date.getUTCMonth()+1, 2)
+      let d = decimal(date.getUTCDate(), 2)
+      let h = decimal(date.getUTCHours(), 2)
+      return `${y}-${m}-${d}-${h}`
+    }
 
     function sample() {
       let clock = Date.now()
       let payload = JSON.stringify({clock})
-      let logfile = `${assets}/plugins/datalog/${slug}.log`
+      let logfile = `${assets}/plugins/datalog/${slug}/${utc(new Date(clock))}.log`
       fs.appendFile(logfile, `${payload}\n`)
     }
 
