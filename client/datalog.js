7d211dc829b6ea2a51666b187aea308ea5eb1ab0
Ward Cunningham <ward@c2.com>
2019-01-09 20:38:28 -0800

    parse markup

--- client/datalog.js
+++ client/datalog.js
@@ -7,14 +7,45 @@
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
       .replace(/\*(.+?)\*/g, '<i>$1</i>')
-      .replace(/\r?\n/g, '<br>')
   };
 
+  function parse(text) {
+    var schedule = {sites:{}, chunk:'hour', interval:5000, keep:24}
+    let output = text.split(/\r?\n/).map (line => {
+      var m
+      if (m = line.match(/^MINUTE (\d+)$/)) {
+        schedule.chunk = 'minute'
+        schedule.interval = 3600000 / m[1]
+      } else if (m = line.match(/^HOUR (\d+)$/)) {
+        schedule.chunk = 'hour'
+        schedule.interval = 3600000 / m[1]
+      } else if (m = line.match(/^DAY (\d+)$/)) {
+        schedule.chunk = 'day'
+        schedule.interval = 3600000*24 / m[1]
+      } else if (m = line.match(/^WEEK (\d+)$/)) {
+        schedule.chunk = 'week'
+        schedule.interval = 3600000*24 / m[1]
+      } else if (m = line.match(/^KEEP (\d+)$/)) {
+        schedule.keep = m[1]*1
+      } else if (m = line.match(/^SENSOR (\w+) (https?:\S+)$/)) {
+        schedule.sites[m[1]] = m[2]
+        line = `SENSOR <a href="${m[2]}" target=_blank>${m[1]} <img src="/images/external-link-ltr-icon.png"></a>`
+      } else {
+        line = `<font color=gray>${expand(line)}</font>`
+      }
+      console.log(schedule)
+      return line
+    }).join('<br>')
+    return {output, schedule}
+  }
+
   function emit($item, item) {
+    parsed = parse(item.text)
     $item.append(`
-      <p style="background-color:#eee;padding:15px;">
-        ${expand(item.text)}
-      </p>`);
+      <div style="background-color:#eee; padding:15px; margin-block-start:1em; margin-block-end:1em;">
+        ${parsed.output}
+        <center><button disabled>wait</button></center>
+      </div>`);
   };
 
   function bind($item, item) {
